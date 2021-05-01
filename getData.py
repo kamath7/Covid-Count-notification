a748c50967624015d7b59941f8635de772852fe4
kamath7 <adithyakamath96@gmail.com>
2021-05-01 18:48:57 +0530

    Set the data retrieval piece properly

--- getData.py
+++ getData.py
@@ -7,7 +7,9 @@ def getData():
     karnatakaCount = apiData["data"]["karnatakaCount"]
     bangaloreCount = apiData["data"]["bangaloreCount"]
     mangaloreCount = apiData["data"]["mangaloreCount"]
-    kasargodCount = apiData["data"]["kasargodCount"]
     udupiCount = apiData["data"]["udupiCount"]
+    mangaloreDeath = apiData["data"]["mangaloreDeath"] if type(
+        int(apiData["data"]["mangaloreDeath"])) else 0
+    kasargodCount = apiData["data"]["kasargodCount"]
     indiaDailyConfirmed = apiData["data"]["indiaDailyConfirmed"]
-    return {'karnatakaCount': karnatakaCount, 'bangaloreCount': bangaloreCount, 'mangaloreCount': mangaloreCount,'kasargodCount':kasargodCount ,'udupiCount': udupiCount, 'indiaDailyConfirmed': indiaDailyConfirmed}
+    return {'karnatakaCount': karnatakaCount, 'bangaloreCount': bangaloreCount, 'mangaloreCount': mangaloreCount, 'kasargodCount': kasargodCount, 'mangaloreDeath': mangaloreDeath, 'udupiCount': udupiCount, 'indiaDailyConfirmed': indiaDailyConfirmed}
